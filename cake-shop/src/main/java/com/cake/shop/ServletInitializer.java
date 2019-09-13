e2cf0abdaee32e32633f7e6cc643e09aa07f8a18
Buffer-GZH <48354334+AllStarGH@users.noreply.github.com>
2019-09-13 14:18:32 +0800

    Update ServletInitializer.java

--- cake-shop/src/main/java/com/cake/shop/ServletInitializer.java
+++ cake-shop/src/main/java/com/cake/shop/ServletInitializer.java
@@ -3,6 +3,7 @@ package com.cake.shop;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/** servlet初始化,没有本类就加载不了项目 */
 public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
